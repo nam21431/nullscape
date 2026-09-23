bdea6cfb215444e1dfbbad090d473d2d8c9fb493
nam21431 <phanvinhnam1801@gmail.com>
2026-09-23 22:13:38 +0700

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -13,7 +13,7 @@ export default {
         <main v-else class="page-roulette">
             <div class="sidebar">
                 <p class="type-label-md" style="color: #aaa">
-                    Shameless copy of the Extreme Demon Roulette by <a href="https://matcool.github.io/extreme-demon-roulette/" target="_blank">matcool</a>.
+                    Shameless copy of the Extreme Demon Roulette by <a href="https://www.youtube.com/@crash-l6c" target="_blank">mrroller</a>.
                 </p>
                 <form class="options">
                     <div class="check">
